47ef92844e8f2d603592b9f6061cce1b1b43e5a1
Aveek Saha <aveek.s98@gmail.com>
2023-03-15 14:24:34 -0400

    Update user model

--- backend/model/User.js
+++ backend/model/User.js
@@ -3,6 +3,7 @@ const { Schema, model } = mongoose;
 
 const userSchema = new Schema({
     user_name: String,
+    user_email: String,
     auth_token: String,
     follows_boards: [{ type: Schema.Types.ObjectId, ref: "MovieBoard" }],
     liked_reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
