f09866ab37a6f41a00b784f407da47c5eee3f0ca
Aveek Saha <aveek.s98@gmail.com>
2023-03-15 13:08:27 -0400

    Add route files

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(3000, () => {
+    console.log(`Server Started at ${3000}`);
+});
+
+
