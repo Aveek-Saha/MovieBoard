231ca66b54f6cb768d1af06aa5a130ba7297e9a4
Aveek Saha <aveek.s98@gmail.com>
2023-04-25 13:06:30 -0400

    Add star rating to review

--- components/reviews/CreateReview.jsx
+++ components/reviews/CreateReview.jsx
@@ -2,12 +2,15 @@
 
 import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faStar as faStarFill } from "@fortawesome/free-solid-svg-icons";
+import { faStar as faStarEmpty } from "@fortawesome/free-regular-svg-icons";
 
 export default function CreateReview({ tmdb_id }) {
     const [heading, setHeading] = useState("");
     const [body, setBody] = useState("");
     const router = useRouter();
-    const rating = 8;
+    // const rating = 8;
 
     const create = async (e) => {
         e.preventDefault();
@@ -28,10 +31,34 @@ export default function CreateReview({ tmdb_id }) {
 
         router.push(`/review/${tmdb_id}`);
     };
+    const [rating, setRating] = useState(0);
+    const [hover, setHover] = useState(0);
     return (
         <div className="col-6">
             <form onSubmit={(e) => create(e)}>
                 <h3>New Review</h3>
+                <div className="star-rating mt-3 mb-3">
+                    {[...Array(10)].map((star, index) => {
+                        index += 1;
+                        return (
+                            <span
+                                key={index}
+                                onClick={() => setRating(index)}
+                                onMouseEnter={() => setHover(index)}
+                                onMouseLeave={() => setHover(rating)}
+                            >
+                                <FontAwesomeIcon
+                                    icon={
+                                        index <= (hover || rating)
+                                            ? faStarFill
+                                            : faStarEmpty
+                                    }
+                                    className="me-1"
+                                />
+                            </span>
+                        );
+                    })}
+                </div>
                 <div className="mb-3">
                     <input
                         className="form-control search"
