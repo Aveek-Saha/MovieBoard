4be838a41975f84aa59c73082d35e54b43d99b01
Aveek Saha <aveek.s98@gmail.com>
2023-05-05 19:44:12 -0400

    Change logout link

--- components/auth/Logout.jsx
+++ components/auth/Logout.jsx
@@ -3,16 +3,18 @@
 import { signOut, useSession } from "next-auth/react";
 
 export default function Logout() {
-    const { data: session, status } = useSession()
+    const { data: session, status } = useSession();
     if (session) {
         return (
             <>
                 <img
                     src={session.user.image}
-                    className="rounded-circle img-thumbnail border-0 p-0 me-2"
+                    className="rounded-circle img-thumbnail border-0 p-0 me-2 link link_img img-fluid"
                     width={35}
                 />
-                <a onClick={() => signOut()}>Logout</a>
+                <a className="link" onClick={() => signOut()}>
+                    Logout
+                </a>
             </>
         );
     }
