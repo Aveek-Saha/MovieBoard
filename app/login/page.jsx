a7e4d1056880c3bb774b9e86035216555db62635
Aveek Saha <aveek.s98@gmail.com>
2023-05-23 02:17:48 -0400

    Prevent relogin

--- app/login/page.jsx
+++ app/login/page.jsx
@@ -1,12 +1,22 @@
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+
 import LoginList from "@/components/auth/LoginList";
 
-export default function Login() {
+export default async function Login() {
+    const session = await getServerSession(authOptions);
+
     return (
         <div className="row justify-content-center">
-            <h2 className="text-center">Login / Sign up</h2>
-            <div className="col-8 col-lg-3 col-md-4 col-sm-6 col-xs-8 d-flex justify-content-center">
-                <LoginList />
-            </div>
+            {session && <h2 className="text-center">Already logged in</h2>}
+            {!session && (
+                <>
+                    <h2 className="text-center">Login / Sign up</h2>
+                    <div className="col-8 col-lg-3 col-md-4 col-sm-6 col-xs-8 d-flex justify-content-center">
+                        <LoginList />
+                    </div>
+                </>
+            )}
         </div>
     );
 }
